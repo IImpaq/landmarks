af60fcc996a1dc4c5a964364b6036464765e3d2e
Marcus Gugacs <iimpaq@proton.me>
2023-09-28 18:57:09 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Marcus Gugacs on 28.09.23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
