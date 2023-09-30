df1001f451ec1c5ff214719d227bf8864b1fba04
Marcus Gugacs <iimpaq@proton.me>
2023-09-30 10:40:39 +0200

    Composed complex interfaces

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,36 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Marcus Gugacs on 30.09.23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        NavigationView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Featured")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+        .environmentObject(ModelData())
+}
